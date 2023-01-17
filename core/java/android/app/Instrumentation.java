738ccea21c5391929bcaabcf936bab74164579b0
Sergey Vasiliev <sdv22@live.ru>
2023-01-17 14:43:58 +0300

    Fix build after merge
    Change-Id: I4d24c25b28fea737e9194ae3a048baa8817b8ada

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -64,12 +64,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
 
-<<<<<<< HEAD
-//import com.android.internal.util.crdroid.PixelPropsUtils;
 import com.android.internal.baikalos.BaikalSpoofer;
-=======
-import com.android.internal.util.crdroid.PixelPropsUtils;
->>>>>>> crdroid/13.0
 
 /**
  * Base class for implementing application instrumentation code.  When running
