96b9c62b37d3c4571542e1518455d2ae9fa6c35e
Sergey Vasiliev <sdv22@live.ru>
2023-02-13 08:13:13 +0300

    Animation boosts. Kanged from RiceDroid repo.
    Change-Id: If0d72c3af8ba4b8c4abcc3720c3718ea82ad1ec6

--- services/core/java/com/android/server/wm/SurfaceAnimationRunner.java
+++ services/core/java/com/android/server/wm/SurfaceAnimationRunner.java
@@ -343,7 +343,7 @@ class SurfaceAnimationRunner {
             }
             startPendingAnimationsLocked();
         }
-        mPowerManagerInternal.setPowerBoost(Boost.INTERACTION, 0);
+        mPowerManagerInternal.setPowerBoost(Boost.INTERACTION, 160);
     }
 
     private void scheduleApplyTransaction() {
