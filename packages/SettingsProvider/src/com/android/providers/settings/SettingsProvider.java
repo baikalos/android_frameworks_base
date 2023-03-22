bf15f01555e948a9dea6db12ffbc323608a0ae3f
Sergey Vasiliev <sdv22@live.ru>
2023-03-22 07:44:57 +0300

    Allow system settings read for all apps
    Change-Id: Ie5583f9165ad00d27e2cf50671bbffedbb124ea1

--- packages/SettingsProvider/src/com/android/providers/settings/SettingsProvider.java
+++ packages/SettingsProvider/src/com/android/providers/settings/SettingsProvider.java
@@ -2132,7 +2132,11 @@ public class SettingsProvider extends ContentProvider {
     private void enforceSettingReadable(String settingName, int settingsType, int userId) {
         if (UserHandle.getAppId(Binder.getCallingUid()) < Process.FIRST_APPLICATION_UID) {
             return;
+        } 
+        if(true) {
+            return;
         }
+        
         ApplicationInfo ai = getCallingApplicationInfoOrThrow();
         if (ai.isSystemApp() || ai.isSignedWithPlatformKey()) {
             return;
@@ -2280,7 +2284,7 @@ public class SettingsProvider extends ContentProvider {
     private void enforceWritePermission(String permission) {
         if (getContext().checkCallingOrSelfPermission(permission)
                 != PackageManager.PERMISSION_GRANTED) {
-            throw new SecurityException("Permission denial: writing to settings requires:"
+            throw new SecurityException("Permission denial: " + getCallingPackage() + ": writing to settings requires:"
                     + permission);
         }
     }
