0502b6d5cc367b1ffcc51df5a85c7ec1787d50a4
Sergey Vasiliev <sdv22@live.ru>
2023-06-03 11:12:30 +0300

    Dump freezer status
    Change-Id: If8701cedc1e5d19b8ba191a05ec5fddbd54afa7a

--- services/core/java/com/android/server/am/ProcessList.java
+++ services/core/java/com/android/server/am/ProcessList.java
@@ -1715,8 +1715,8 @@ public final class ProcessList {
                     if (pm.checkPermission(Manifest.permission.INSTALL_PACKAGES,
                             app.info.packageName, userId)
                             == PackageManager.PERMISSION_GRANTED) {
-                        Slog.i(TAG, app.info.packageName + " is exempt from freezer");
-                        app.mOptRecord.setFreezeExempt(true);
+                        //Slog.i(TAG, app.info.packageName + " is exempt from freezer");
+                        //app.mOptRecord.setFreezeExempt(true);
                     }
                 } catch (RemoteException e) {
                     throw e.rethrowAsRuntimeException();
@@ -4383,9 +4383,15 @@ public final class ProcessList {
                 foreground = 'S';
             } else if (r.mAppProfile.mPinned) {
                 foreground = 'P';
+            } else if (r.mOptRecord.isFrozen()) {
+                foreground = 'F';
+            } else if (r.mOptRecord.isPendingFreeze()) {
+                foreground = 'f';
             } else {
                 foreground = ' ';
             }
+
+
             String procState = makeProcStateString(state.getCurProcState());
             pw.print(prefix);
             pw.print(r.isPersistent() ? persistentLabel : normalLabel);
