3e378856eccd41101ff22a4ce90a94c1bf8d361d
Sergei Vasilev <svasiliev22@yandex.ru>
2023-11-04 19:03:24 +0300

    Add BAIKAL_DEBUG_IDLE constant
    Change-Id: If2a4f67dac8bb35b2e03ee5c77b30ca3663cf5e8

--- core/java/com/android/internal/baikalos/BaikalConstants.java
+++ core/java/com/android/internal/baikalos/BaikalConstants.java
@@ -66,6 +66,7 @@ public class BaikalConstants {
     public static boolean BAIKAL_DEBUG_POWER = BAIKAL_DEBUG | false;
     public static boolean BAIKAL_DEBUG_JOBS = BAIKAL_DEBUG | false;
     public static boolean BAIKAL_DEBUG_WAKELOCKS = BAIKAL_DEBUG | false;
+    public static boolean BAIKAL_DEBUG_IDLE = BAIKAL_DEBUG | false;
 
 
     public static final int DEBUG_MASK_ALL = 0x0001;
@@ -94,6 +95,7 @@ public class BaikalConstants {
     public static final int DEBUG_MASK_POWER = 0x100000;
     public static final int DEBUG_MASK_JOBS = 0x200000;
     public static final int DEBUG_MASK_WAKELOCKS = 0x400000;
+    public static final int DEBUG_MASK_IDLE = 0x800000;
 
     public static void Logv(boolean type, int uid,String tag, String msg) {
         if( type ) { Slog.v(tag,msg); return; }
