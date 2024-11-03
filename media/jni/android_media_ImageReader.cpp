cb99aeed49a24c83d884d8136d56decd951117d8
Sergei Vasilev <svasiliev22@yandex.ru>
2024-11-03 17:59:35 +0300

    Check ImageBuffer state
    Change-Id: Idd53e7dce58156f760677db758bf7e17a4017456

--- media/jni/android_media_ImageReader.cpp
+++ media/jni/android_media_ImageReader.cpp
@@ -900,12 +900,22 @@ static jobjectArray Image_createSurfacePlanes(JNIEnv* env, jobject thiz,
 static jint Image_getWidth(JNIEnv* env, jobject thiz)
 {
     BufferItem* buffer = Image_getBufferItem(env, thiz);
+    if (buffer == NULL) {
+        jniThrowException(env, "java/lang/IllegalStateException",
+            "Image is not initialized");
+        return static_cast<jint>(0);
+    }
     return getBufferWidth(buffer);
 }
 
 static jint Image_getHeight(JNIEnv* env, jobject thiz)
 {
     BufferItem* buffer = Image_getBufferItem(env, thiz);
+    if (buffer == NULL) {
+        jniThrowException(env, "java/lang/IllegalStateException",
+            "Image is not initialized");
+        return static_cast<jint>(0);
+    }
     return getBufferHeight(buffer);
 }
 
@@ -915,7 +925,6 @@ static jint Image_getFenceFd(JNIEnv* env, jobject thiz)
     if ((buffer != NULL) && (buffer->mFence.get() != NULL)){
         return buffer->mFence->get();
     }
-
     return -1;
 }
 
@@ -926,6 +935,12 @@ static jint Image_getFormat(JNIEnv* env, jobject thiz, jint readerFormat)
         return static_cast<jint>(PublicFormat::PRIVATE);
     } else {
         BufferItem* buffer = Image_getBufferItem(env, thiz);
+        if (buffer == NULL) {
+            jniThrowException(env, "java/lang/IllegalStateException",
+                "Image is not initialized");
+            return -1;
+            
+        }
         int readerHalFormat = mapPublicFormatToHalFormat(static_cast<PublicFormat>(readerFormat));
         int32_t fmt = applyFormatOverrides(
                 buffer->mGraphicBuffer->getPixelFormat(), readerHalFormat);
@@ -944,6 +959,11 @@ static jint Image_getFormat(JNIEnv* env, jobject thiz, jint readerFormat)
 
 static jobject Image_getHardwareBuffer(JNIEnv* env, jobject thiz) {
     BufferItem* buffer = Image_getBufferItem(env, thiz);
+    if (buffer == NULL) {
+        jniThrowException(env, "java/lang/IllegalStateException",
+                "Image is not initialized");
+        return NULL;
+    }
     AHardwareBuffer* b = AHardwareBuffer_from_GraphicBuffer(buffer->mGraphicBuffer.get());
     // don't user the public AHardwareBuffer_toHardwareBuffer() because this would force us
     // to link against libandroid.so
