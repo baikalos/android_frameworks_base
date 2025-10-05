ef4b2ad690c6ab027e07419342bdb00f65a61f20
Sergei Vasilev <svasiliev22@yandex.ru>
2025-10-05 09:00:46 +0300

    Reduce AnyMotion time to 10s
    Change-Id: I9bc497a5269ebd1132948a38b03419a95dff3d2e

--- apex/jobscheduler/service/java/com/android/server/AnyMotionDetector.java
+++ apex/jobscheduler/service/java/com/android/server/AnyMotionDetector.java
@@ -73,7 +73,7 @@ public class AnyMotionDetector {
     private static final long ORIENTATION_MEASUREMENT_INTERVAL_MILLIS = 5000;
 
     /** The maximum duration we will hold a wakelock to determine stationary status. */
-    private static final long WAKELOCK_TIMEOUT_MILLIS = 30000;
+    private static final long WAKELOCK_TIMEOUT_MILLIS = 10000;
 
     /**
      * The duration in milliseconds after which an orientation measurement is considered
