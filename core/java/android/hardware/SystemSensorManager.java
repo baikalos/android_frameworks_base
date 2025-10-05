f3dd79f7382ad8a216a36747e0c3e18e1ad6ed34
Sergei Vasilev <svasiliev22@yandex.ru>
2025-10-05 09:18:32 +0300

    Add more sensors to blocklist
    Change-Id: I82a0b55c93526aea12e94025fcaf5e5620f4801f

--- core/java/android/hardware/SystemSensorManager.java
+++ core/java/android/hardware/SystemSensorManager.java
@@ -283,6 +283,15 @@ public class SystemSensorManager extends SensorManager {
             int sensortype = sensor.getType();
             if (sensortype == Sensor.TYPE_SIGNIFICANT_MOTION ||
                     sensortype == Sensor.TYPE_ACCELEROMETER ||
+                    sensortype == Sensor.TYPE_GRAVITY ||
+                    sensortype == Sensor.TYPE_GYROSCOPE ||
+                    sensortype == Sensor.TYPE_LINEAR_ACCELERATION ||
+                    sensortype == Sensor.TYPE_ROTATION_VECTOR ||
+                    sensortype == Sensor.TYPE_GYROSCOPE_UNCALIBRATED ||
+                    sensortype == Sensor.TYPE_GEOMAGNETIC_ROTATION_VECTOR ||
+                    sensortype == Sensor.TYPE_STATIONARY_DETECT ||
+                    sensortype == Sensor.TYPE_MOTION_DETECT ||
+                    sensortype == Sensor.TYPE_ACCELEROMETER_UNCALIBRATED ||
                     sensortype == Sensor.TYPE_LINEAR_ACCELERATION) {
                 String pkgName = mContext.getPackageName();
                 if (isBlockedApp(pkgName)) {
@@ -307,7 +316,9 @@ public class SystemSensorManager extends SensorManager {
         if (Settings.Global.getInt(mContext.getContentResolver(),
                 Settings.Global.BAIKALOS_AGGRESSIVE_DEVICE_IDLE, 0) == 1 ) {
 
-            if (sensortype == Sensor.TYPE_SIGNIFICANT_MOTION) {
+            if (sensortype == Sensor.TYPE_SIGNIFICANT_MOTION ||
+                    sensortype == Sensor.TYPE_STATIONARY_DETECT ||
+                    sensortype == Sensor.TYPE_MOTION_DETECT ) {
                     Log.w(TAG, "Preventing " + pkgName + " from draining battery using " +
                             "significant motion sensor");
                     return true;
@@ -379,6 +390,30 @@ public class SystemSensorManager extends SensorManager {
                     + MAX_LISTENER_COUNT);
         }
 
+        if (Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.SENSOR_BLOCK, 0) == 1) {
+            int sensortype = sensor.getType();
+            if (sensortype == Sensor.TYPE_SIGNIFICANT_MOTION ||
+                    sensortype == Sensor.TYPE_ACCELEROMETER ||
+                    sensortype == Sensor.TYPE_GRAVITY ||
+                    sensortype == Sensor.TYPE_GYROSCOPE ||
+                    sensortype == Sensor.TYPE_LINEAR_ACCELERATION ||
+                    sensortype == Sensor.TYPE_ROTATION_VECTOR ||
+                    sensortype == Sensor.TYPE_GYROSCOPE_UNCALIBRATED ||
+                    sensortype == Sensor.TYPE_GEOMAGNETIC_ROTATION_VECTOR ||
+                    sensortype == Sensor.TYPE_STATIONARY_DETECT ||
+                    sensortype == Sensor.TYPE_MOTION_DETECT ||
+                    sensortype == Sensor.TYPE_ACCELEROMETER_UNCALIBRATED ||
+                    sensortype == Sensor.TYPE_LINEAR_ACCELERATION) {
+                String pkgName = mContext.getPackageName();
+                if (isBlockedApp(pkgName)) {
+                    Log.w(TAG, "Preventing " + pkgName + " from draining battery using " +
+                            sensor.getStringType());
+                    return false;
+                }
+            }
+        }
+
         int sensortype = sensor.getType();
 
         String pkgName = mContext.getPackageName();
@@ -393,7 +428,9 @@ public class SystemSensorManager extends SensorManager {
         if (Settings.Global.getInt(mContext.getContentResolver(),
                 Settings.Global.BAIKALOS_AGGRESSIVE_DEVICE_IDLE, 0) == 1 ) {
 
-            if (sensortype == Sensor.TYPE_SIGNIFICANT_MOTION) {
+            if (sensortype == Sensor.TYPE_SIGNIFICANT_MOTION ||
+                    sensortype == Sensor.TYPE_STATIONARY_DETECT ||
+                    sensortype == Sensor.TYPE_MOTION_DETECT ) {
                     Log.w(TAG, "Preventing " + pkgName + " from draining battery using " +
                             "significant motion sensor");
                     return true;
