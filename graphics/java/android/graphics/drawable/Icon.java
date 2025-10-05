99d6e4d547f0711c089a88e46c539dd4ece3bc12
Sergei Vasilev <svasiliev22@yandex.ru>
2025-10-05 09:25:27 +0300

    Fix icon scaler
    Change-Id: I82bfd519da02b3238ba938a5313e644be889ff73

--- graphics/java/android/graphics/drawable/Icon.java
+++ graphics/java/android/graphics/drawable/Icon.java
@@ -983,11 +983,18 @@ public final class Icon implements Parcelable {
         if (bitmapWidth > maxWidth || bitmapHeight > maxHeight) {
             float scale = Math.min((float) maxWidth / bitmapWidth,
                     (float) maxHeight / bitmapHeight);
-            bitmap = Bitmap.createScaledBitmap(bitmap,
-                    Math.max(1, (int) (scale * bitmapWidth)),
-                    Math.max(1, (int) (scale * bitmapHeight)),
-                    true /* filter */);
-        }
+            int scaledWidth = (int) (scale * bitmapWidth);
+            int scaledHeight = (int) (scale * bitmapHeight);
+            if (scaledWidth > maxWidth) {
+                scaledWidth = maxWidth;
+            }
+            if (scaledHeight > maxHeight) {
+                scaledHeight = maxHeight;
+            }
+            bitmap = Bitmap.createScaledBitmap(bitmap, 
+                    Math.max(1, scaledWidth), 
+                    Math.max(1, scaledHeight), 
+                    true /* filter */);        }
         return bitmap;
     }
 
